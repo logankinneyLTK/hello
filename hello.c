2ea25686761533f5be1f767a8950d1299b2b8358
logankinneyLTK <lkinney5@huskers.unl.edu>
2022-08-25 20:05:39 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Logan Kinney
+ *         lkinney5@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Logan Kinney\n");
+  printf("Computer Science\n");
+  return 0;
+}
